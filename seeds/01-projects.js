bac9970ddb8e45ae1c0a8c6d98cc0a5a14e86c18
mgomri <mel77gomri@gmail.com>
2020-05-15 11:41:36 -0400

    data base created and seeded

--- seeds/01-projects.js
+++ seeds/01-projects.js
@@ -1,13 +1,24 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('projects').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('projects').insert([
+        {
+          name: 'Steal the sun', 
+          completed:'false'
+        },
+        { 
+
+          name: 'Fly like an eagle into the future', 
+          completed:false
+        },
+        {
+          name: 'Get a life', 
+          completed:false
+        }
+       
       ]);
     });
 };
